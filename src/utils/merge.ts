a0759e02fd1b40c86e9799c3ee6fb2d43e31bf98
Olivier Louvignes <olivier@mgcrea.io>
2021-03-23 12:25:24 +0100

    feat(update): minor changes

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,18 +1,14 @@
 import { KiteRequestInit, KiteOptions, KiteHooks, KiteHeadersInit } from 'src/core/KiteClient.js';
 
-export const mergeOptions = (options: KiteRequestInit, source: KiteRequestInit = {}): KiteOptions => {
-  const hooks = mergeHooks(options.hooks, source.hooks);
-  const headers = mergeHeaders(options.headers, source.headers);
-  return { ...options, ...source, hooks, headers };
-};
-
 export const mergeArray = <T>(array?: T[], source?: T[]): T[] => (array ? array.slice() : []).concat(source ?? []);
+
 export const mergeHooks = (hooks: Partial<KiteHooks> = {}, source: Partial<KiteHooks> = {}): KiteHooks => {
   return {
     beforeRequest: mergeArray(hooks.beforeRequest, source.beforeRequest),
     afterResponse: mergeArray(hooks.afterResponse, source.afterResponse),
   };
 };
+
 export const mergeHeaders = (headersInit: KiteHeadersInit = {}, source: KiteHeadersInit = {}): Headers => {
   const headers = new Headers(headersInit as HeadersInit);
   if (!source) {
@@ -27,4 +23,10 @@ export const mergeHeaders = (headersInit: KiteHeadersInit = {}, source: KiteHead
     }
   }
   return headers;
+};
+
+export const mergeOptions = (options: KiteRequestInit, source: KiteRequestInit = {}): KiteOptions => {
+  const hooks = mergeHooks(options.hooks, source.hooks);
+  const headers = mergeHeaders(options.headers, source.headers);
+  return { ...options, ...source, hooks, headers };
 };
